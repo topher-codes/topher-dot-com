4245db2387be9dd9dc1f8f7542fcc05c95022f92
Chris <ghost@drednaut.net>
2022-11-08 16:23:40 -0800

    posts
    Signed-off-by: Chris <ghost@drednaut.net>

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,32 @@
+'use client';
+
+import { Posts } from './posts';
+
 export default function BlogPage() {
+	const posts = Posts;
+	function log() {
+		console.log(posts);
+	}
+
 	return (
 		<div>
 			<h1>Blogg</h1>
+			<div className="blog-list">
+				<button onClick={log}>Log</button>
+				<select name="select" id="select"></select>
+			</div>
 		</div>
 	);
 }
+
+// function BlogPosts({ post }: any) {
+// 	const { id, title, description, date } = post || {};
+// 	return (
+// 		<div>
+// 			<p>{id}</p>
+// 			<p>{title}</p>
+// 			<p>{description}</p>
+// 			<p>{date}</p>
+// 		</div>
+// 	);
+// }
---
+++ app/blog/posts.ts
@@ -0,0 +1,26 @@
+export const Posts = [
+	{
+		id: 0,
+		title: 'Hello, Markdown',
+		description: 'A post about the markdown syntax',
+		date: '012322',
+	},
+	{
+		id: 1,
+		title: 'Hello, Markdown',
+		description: 'A post about the markdown syntax',
+		date: '012322',
+	},
+	{
+		id: 2,
+		title: 'Hello, Markdown',
+		description: 'A post about the markdown syntax',
+		date: '012322',
+	},
+	{
+		id: 3,
+		title: 'Hello, Markdown',
+		description: 'A post about the markdown syntax',
+		date: '012322',
+	},
+];
