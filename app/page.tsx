e99a33f47972bf8fc916efadff141480adcde5a9
Chris <ghost@drednaut.net>
2022-11-28 12:03:46 -0800

    updated image url
    Signed-off-by: Chris <ghost@drednaut.net>

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
 			<div className="card" id="card-photo">
 				<div className="card-img">
 					<Image
-						src="https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/295039461_3274098816179609_5255628418250824421_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=6BdGgZX6qLEAX-A8_7P&_nc_ht=scontent-sea1-1.xx&oh=00_AfC85QywsQ2PXjFLxU_B3HL1aYkAzv0lijIu8EssJ6Ibwg&oe=636ABE79"
+						src="https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/295039461_3274098816179609_5255628418250824421_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=4k-mhZ1SlnYAX8rCcCH&_nc_ht=scontent-sea1-1.xx&oh=00_AfAmFohl7GJ2O8KLpi0yaS43g2wK6vEtWcf8PgIEmkqTng&oe=638A6279"
 						width="300"
 						height="300"
 						alt="nothing"
